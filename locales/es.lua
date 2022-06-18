3fc64e3dedf56a08b634833e2c0becf786a2edd7
xdanial <58679481+xdanial@users.noreply.github.com>
2022-06-18 10:51:36 -0400

    Spanish translate.

---
+++ locales/es.lua
@@ -0,0 +1,9 @@
+Locales['es'] = {
+    ["interact"] = "Presiona [~p~E~w~] para abrir el Black Market",
+    ["market_name"] = "Black Market",
+    ["buy"] = "Comprar %s por %s$",
+    ["buyed"] = "Has comprado %s por %s$",
+    ["cant_carry_item"] = "No tienes espacio suficiente para este item.",
+    ["already_has_weapon"] = "Ya tienes este arma en el inventario.",
+    ["no_money"] = "No tienes suficiente dinero."
+}
